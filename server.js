8dbfb8a496949a193a6e86daf492ffd8e3b1442a
Duscicat <duscicat@gmail.com>
2019-02-23 02:53:52 +0700

    #1 add sweets

--- server.js
+++ server.js
@@ -10,10 +10,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 
-app.get("/hello", (req, res) => {
-    res.send({
-        age : 15,
-    });
+const sweets = [
+    { imageBigUrl: "dist/img/tetes_brulees_blueberry_Big.png", imageUrl: "dist/img/tetes_brulees_blueberry.png", name: "Tetes Brulees Blue Raspberry Color", description: "Кислая малина", price: 42 },
+    { imageBigUrl: "dist/img/top_pops_berry_Big.png", imageUrl: "dist/img/top_pops_berry.png", name: "Top Pops Red and Blue Raspberry", description: "Вкусные ретро жевательные ириски", price: 54 },
+    { imageBigUrl: "dist/img/top_pops_watermelon_Big.png", imageUrl: "dist/img/top_pops_watermelon.png", name: "Top Pops Watermelon", description: "Вкусные ретро жевательные ириски", price: 33},
+    { imageBigUrl: "dist/img/tetes_brulees_blueberry_Big.png", imageUrl: "dist/img/tetes_brulees_blueberry.png", name: "Tetes Brulees Blue Raspberry Color", description: "Кислая малина", price: 42 },
+    { imageBigUrl: "dist/img/top_pops_berry_Big.png", imageUrl: "dist/img/top_pops_berry.png", name: "Top Pops Red and Blue Raspberry", description: "Вкусные ретро жевательные ириски", price: 54 },
+    { imageBigUrl: "dist/img/top_pops_watermelon_Big.png", imageUrl: "dist/img/top_pops_watermelon.png", name: "Top Pops Watermelon", description: "Вкусные ретро жевательные ириски", price: 33},
+    { imageBigUrl: "dist/img/tetes_brulees_blueberry_Big.png", imageUrl: "dist/img/tetes_brulees_blueberry.png", name: "Tetes Brulees Blue Raspberry Color", description: "Кислая малина", price: 42 },
+    { imageBigUrl: "dist/img/top_pops_berry_Big.png", imageUrl: "dist/img/top_pops_berry.png", name: "Top Pops Red and Blue Raspberry", description: "Вкусные ретро жевательные ириски", price: 54 },
+    { imageBigUrl: "dist/img/top_pops_watermelon_Big.png", imageUrl: "dist/img/top_pops_watermelon.png", name: "Top Pops Watermelon", description: "Вкусные ретро жевательные ириски", price: 33},
+    { imageBigUrl: "dist/img/tetes_brulees_blueberry_Big.png", imageUrl: "dist/img/tetes_brulees_blueberry.png", name: "Tetes Brulees Blue Raspberry Color", description: "Кислая малина", price: 42 },
+    { imageBigUrl: "dist/img/top_pops_berry_Big.png", imageUrl: "dist/img/top_pops_berry.png", name: "Top Pops Red and Blue Raspberry", description: "Вкусные ретро жевательные ириски", price: 54 },
+    { imageBigUrl: "dist/img/top_pops_watermelon_Big.png", imageUrl: "dist/img/top_pops_watermelon.png", name: "Top Pops Watermelon", description: "Вкусные ретро жевательные ириски", price: 33},
+    { imageBigUrl: "dist/img/tetes_brulees_blueberry_Big.png", imageUrl: "dist/img/tetes_brulees_blueberry.png", name: "Tetes Brulees Blue Raspberry Color", description: "Кислая малина", price: 42 },
+    { imageBigUrl: "dist/img/top_pops_berry_Big.png", imageUrl: "dist/img/top_pops_berry.png", name: "Top Pops Red and Blue Raspberry", description: "Вкусные ретро жевательные ириски", price: 54 },
+    { imageBigUrl: "dist/img/top_pops_watermelon_Big.png", imageUrl: "dist/img/top_pops_watermelon.png", name: "Top Pops Watermelon", description: "Вкусные ретро жевательные ириски", price: 33},
+    { imageBigUrl: "dist/img/tetes_brulees_blueberry_Big.png", imageUrl: "dist/img/tetes_brulees_blueberry.png", name: "Tetes Brulees Blue Raspberry Color", description: "Кислая малина", price: 42 },
+    { imageBigUrl: "dist/img/top_pops_berry_Big.png", imageUrl: "dist/img/top_pops_berry.png", name: "Top Pops Red and Blue Raspberry", description: "Вкусные ретро жевательные ириски", price: 54 },
+    { imageBigUrl: "dist/img/top_pops_watermelon_Big.png", imageUrl: "dist/img/top_pops_watermelon.png", name: "Top Pops Watermelon", description: "Вкусные ретро жевательные ириски", price: 33}
+];
+app.get("/products", (req, res) => {
+    res.send(sweets);
 });
 
 app.listen(3000, () => console.log("started"));
